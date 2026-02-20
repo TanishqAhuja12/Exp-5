9da8f7ca2335f7b8ea88b9b37bfe3c5c0af2641e
Tanishq Ahuja <tanishqahuja122@gmail.com>
2026-02-20 09:23:31 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="card">
+      <h2>Welcome</h2>
+      <h3>Tanishq Ahuja</h3>
+      <p><strong>UID:</strong> 23BAI70611</p>
+      <p>This is the Home Page of Lazy Loading Experiment.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="card">
+      <h2>Home Page</h2>
+      <h3>Tanishq Ahuja</h3>
+      <p><strong>UID:</strong> 23BAI70611 </p>
+      <p>This page is lazy loaded when route is accessed.</p>
+    </div>
+  );
+}
+
+export default Home;
